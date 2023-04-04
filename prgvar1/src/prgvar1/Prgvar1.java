03a85680f368af17ab79466d98ff58ffb7fad665
zb662000 <60846530+zb662000@users.noreply.github.com>
2023-04-04 19:56:53 +0200

    Add files via upload

---
+++ prgvar1/src/prgvar1/Prgvar1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prgvar1;
+
+/**
+ *
+ * @author Zahra
+ */
+public class Prgvar1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("This is my second Java Application");
+       int intValue = 8;
+String strName = "Zahra";
+
+System.out.println(strName);
+System.out.println(intValue);
+
+System.out.println("This is my second appilcation and my name is " +strName+" and the time is "
++intValue+" pm");
+
+
+    }
+    
+}
