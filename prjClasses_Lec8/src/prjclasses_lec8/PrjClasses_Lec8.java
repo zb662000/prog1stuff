20bb7fad1ac6958df3fe561b8a838246d3d78235
zb662000 <60846530+zb662000@users.noreply.github.com>
2023-04-04 19:54:13 +0200

    Add files via upload

---
+++ prjClasses_Lec8/src/prjclasses_lec8/PrjClasses_Lec8.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjclasses_lec8;
+
+/**
+ *
+ * @author Zahra
+ */
+public class PrjClasses_Lec8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        newClass nc = new newClass();
+        
+        nc.HelloClass();
+        System.out.println(nc.hiddenClassvar);
+        
+    }
+    
+}
---
+++ prjClasses_Lec8/src/prjclasses_lec8/newClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjclasses_lec8;
+
+/**
+ *
+ * @author Zahra
+ */
+public class newClass {
+    
+    public void HelloClass(){
+        System.out.println("Hello from the class next door");
+    }
+    public String hiddenClassvar = "Today is Friday!";
+}
