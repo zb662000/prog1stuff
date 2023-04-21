83ea2bd10e882abeaf96eda61820c4ad731e7b9a
zb662000 <60846530+zb662000@users.noreply.github.com>
2023-04-21 18:05:02 +0200

    Add files via upload

---
+++ SimpleIf/src/simpleif/SimpleIf.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleif;
+
+/**
+ *
+ * @author Zahra
+ */
+
+//              == is equal to
+		// != is not equal to
+		// <= less than or equal to
+		// < less than
+		// > greater than
+		// >= greater than or equal to
+		
+public class SimpleIf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x, y;
+        double a,b, c;
+        
+        x = 3;
+        y = 4;
+        
+        if (x < y) System.out.println("x is less than y");
+         if (x == y) System.out.println("x is equal to y");
+         
+         a = 3.45;
+         b = 4.25;
+         c = 3.45;
+         
+         if (a == c) System.out.println("a is equal to c");
+          if (b >= a) System.out.println("b is greater than or equal to a");
+          if (a != b ) System.out.println("a is not equal to b");
+          
+    }
+    
+}
